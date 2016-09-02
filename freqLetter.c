b58f62499a5a90d5f56f0800205a8f252a4efd67
HenryQuan <natequan@hotmail.com>
2016-09-02 19:47:30 +1000

    Updating main functions
    Checking bugs.

--- freqLetter.c
+++ freqLetter.c
@@ -1,7 +1,7 @@
 /* This is a question from my computing test.
 The question is that get the most frequent letter from a short text.
 Example: I am HenryQuan.
-It prints "N shows 2 times".
+It prints "N occurs 2 times".
 
 02/09/2016
 HenryQuan
@@ -17,7 +17,7 @@ HenryQuan
 
 int letterToNumber(int letter);
 int numberToLetter(int letter);
-void getMax(int alphabet[ALPHABET]);
+int getMax(int alphabet[ALPHABET]);
 void countMostFreqLetter(int alphabet[ALPHABET]);
 
 int main(int argc, char *argv[]) {
@@ -53,10 +53,37 @@ int numberToLetter(int letter) {
 	
 }
 
-void getMax(int alphabet[ALPHABET]) {
+int getMax(int alphabet[ALPHABET]) {
+	
+	// Set the first element to be biggest fro comparing.
+	int max = alphabet[0];
+	
+	int i = 1;
+	while (i < ALPHABET) {
+		if (max < alphabet[i]) {
+			max = alphabet[i];
+		}
+	}
+	
+	return max;
 	
 }
 
 void countMostFreqLetter(int alphabet[ALPHABET]) {
+	
+	int ch;
+	int number = 0;
+	while ((ch = getchar()) != '\n') {
+		if ((ch >= UPPER_START && ch <= UPPER_END)
+		|| (ch >= LOWER_START && ch <= LOWER_END)) {
+			// To see if it is a alphabet.
+			number = letterToNumber(ch);
+			alphabet[number]++;
+		}
+	}
+	
+	int max = getMax(alphabet);
+	int character = numberToLetter(max);
+	printf("\n%c occurs %d times\n", character, alphabet[max]);
 	
 }
